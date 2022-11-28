bba47c7c21a51aa7ff69a486698127dc54805ea3
TianYuan <white-sky@qq.com>
2022-11-29 00:03:45 +0800

    select sentenses ok

--- app/src/main/java/com/baidu/paddle/lite/demo/tts/MainActivity.java
+++ app/src/main/java/com/baidu/paddle/lite/demo/tts/MainActivity.java
@@ -22,14 +22,19 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, AdapterView.OnItemSelectedListener {
     public static final int REQUEST_LOAD_MODEL = 0;
     public static final int REQUEST_RUN_MODEL = 1;
     public static final int RESPONSE_LOAD_MODEL_SUCCESSED = 0;
@@ -50,10 +55,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected TextView tvInputSetting;
     protected TextView tvInferenceTime;
 
-    protected Button btn_syn ;
     protected Button btn_play;
-    protected Button btn_pause ;
-    protected Button btn_stop ;
+    protected Button btn_pause;
+    protected Button btn_stop;
 
 
     // protected Switch mSwitch;
@@ -67,23 +71,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     protected Predictor predictor = new Predictor();
     private MediaPlayer mediaPlayer = new MediaPlayer();
-    private String wavName = "hf.wav";
+    private String wavName = "hp.wav";
     private String wavFile = Environment.getExternalStorageDirectory() + File.separator + wavName;
     private String AMmodelName = "fastspeech2_csmsc_arm.nb";
     private String VOCmodelName = "mb_melgan_csmsc_arm.nb";
-    private float[] phones = {261, 231, 175, 116, 179, 262, 44, 154, 126, 177, 19, 262, 42, 241, 72, 177, 56, 174, 245, 37, 186, 37, 49, 151, 127, 69, 19, 179, 72, 69, 4, 260, 126, 177, 116, 151, 239, 153, 141};
-//    private float[] phones = {261, 231, 175, 116, 179, 262, 44, 154, 126, 177, 19, 262};
-    int sampleRate=24000;
+    private float[] phones = {};
+    private float[][] sentencesToChoose = {
+            // 009901 昨日，这名“伤者”与医生全部被警方依法刑事拘留。
+            {261, 231, 175, 116, 179, 262, 44, 154, 126, 177, 19, 262, 42, 241, 72, 177, 56, 174, 245, 37, 186, 37, 49, 151, 127, 69, 19, 179, 72, 69, 4, 260, 126, 177, 116, 151, 239, 153, 141},
+            // 009902 钱伟长想到上海来办学校是经过深思熟虑的。
+            {174, 83, 213, 39, 20, 260, 89, 40, 30, 177, 22, 71, 9, 153, 8, 37, 17, 260, 251, 260, 99, 179, 177, 116, 151, 125, 70, 233, 177, 51, 176, 108, 177, 184, 153, 242, 40, 45},
+            // 009903 她见我一进门就骂，吃饭时也骂，骂得我抬不起头。
+            {182, 2, 151, 85, 232, 73, 151, 123, 154, 52, 151, 143, 154, 5, 179, 39, 113, 69, 17, 177, 114, 105, 154, 5, 179, 154, 5, 40, 45, 232, 182, 8, 37, 186, 174, 74, 182, 168},
+            // 009904 李述德在离开之前，只说了一句“柱驼杀父亲了”。
+            {153, 74, 177, 186, 40, 42, 261, 10, 153, 73, 152, 7, 262, 113, 174, 83, 179, 262, 115, 177, 230, 153, 45, 73, 151, 242, 180, 262, 186, 182, 231, 177, 2, 69, 186, 174, 124, 153, 45},
+            // 009905 这种车票和保险单捆绑出售属于重复性购买。
+            {262, 44, 262, 163, 39, 41, 173, 99, 71, 42, 37, 28, 260, 84, 40, 14, 179, 152, 220, 37, 21, 39, 183, 177, 170, 179, 177, 185, 240, 39, 162, 69, 186, 260, 128, 70, 170, 154, 9},
+            // 009906 戴佩妮的男友西米露接唱情歌，让她非常开心。
+            {40, 10, 173, 49, 155, 72, 40, 45, 155, 15, 142, 260, 72, 154, 74, 153, 186, 179, 151, 103, 39, 22, 174, 126, 70, 41, 179, 175, 22, 182, 2, 69, 46, 39, 20, 152, 7, 260, 120},
+            // 009907 观大势、谋大局、出大策始终是该院的办院方针。
+            {70, 199, 40, 5, 177, 116, 154, 168, 40, 5, 151, 240, 179, 39, 183, 40, 5, 38, 44, 179, 177, 115, 262, 161, 177, 116, 70, 7, 247, 40, 45, 37, 17, 247, 69, 19, 262, 51},
+            // 009908 他们骑着摩托回家，正好为农忙时的父母帮忙。
+            {182, 2, 154, 55, 174, 73, 262, 45, 154, 157, 182, 230, 71, 212, 151, 77, 180, 262, 59, 71, 29, 214, 155, 162, 154, 20, 177, 114, 40, 45, 69, 186, 154, 185, 37, 19, 154, 20},
+            // 009909 但是因为还没到退休年龄，只能掰着指头捱日子。
+            {40, 17, 177, 116, 120, 214, 71, 8, 154, 47, 40, 30, 182, 214, 260, 140, 155, 83, 153, 126, 180, 262, 115, 155, 57, 37, 7, 262, 45, 262, 115, 182, 171, 8, 175, 116, 261, 112},
+            // 009910 这几天雨水不断，人们恨不得待在家里不出门。
+            {262, 44, 151, 74, 182, 82, 240, 177, 213, 37, 184, 40, 202, 180, 175, 52, 154, 55, 71, 54, 37, 186, 40, 42, 40, 7, 261, 10, 151, 77, 153, 74, 37, 186, 39, 183, 154, 52}
+
+    };
+    int sampleRate = 24000;
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn_syn:
-                if (onLoadModelSuccessed()) {
-                    Log.e(TAG,"R.id.btn_syn");
-                    runModel();
-                }
-                break;
             case R.id.btn_play:
                 if (!mediaPlayer.isPlaying()) {
                     mediaPlayer.start();
@@ -104,6 +124,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
     private void initMediaPlayer() {
         try {
             File file = new File(wavFile);
@@ -122,9 +143,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onPrepared(MediaPlayer player){
+    public void onPrepared(MediaPlayer player) {
         player.start();
-        Log.d("TAG","prepared");
+        Log.d("TAG", "prepared");
     }
 
     @Override
@@ -142,12 +163,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_syn = findViewById(R.id.btn_syn);
+        //初始化控件
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        //建立数据源
+        String[] sentences = getResources().getStringArray(R.array.text);
+        //建立Adapter并且绑定数据源
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sentences);
+        //第一个参数表示在哪个Activity上显示，第二个参数是系统下拉框的样式，第三个参数是数组。
+        spinner.setAdapter(adapter);//绑定Adapter到控件
+        spinner.setOnItemSelectedListener(this);
+
         btn_play = findViewById(R.id.btn_play);
-        btn_pause =  findViewById(R.id.btn_pause);
+        btn_pause = findViewById(R.id.btn_pause);
         btn_stop = findViewById(R.id.btn_stop);
 
-        btn_syn.setOnClickListener(this);
         btn_play.setOnClickListener(this);
         btn_pause.setOnClickListener(this);
         btn_stop.setOnClickListener(this);
@@ -263,7 +292,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void loadModel() {
-        Log.e(TAG,"Loading model...");
+        Log.e(TAG, "Loading model...");
         pbLoadModel = ProgressDialog.show(this, "", "Loading model...", false, false);
         sender.sendEmptyMessage(REQUEST_LOAD_MODEL);
     }
@@ -304,7 +333,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         if (ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission. WRITE_EXTERNAL_STORAGE }, 1);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         } else {
             // 初始化MediaPlayer
             initMediaPlayer();
@@ -377,5 +406,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return false;
         }
         return true;
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (position > 0) {
+            phones = sentencesToChoose[position - 1];
+            runModel();
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
